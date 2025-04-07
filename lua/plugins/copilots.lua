86f314c3e962be7bf450b7d129130ac563337c37
Daniel Dussan <daniel2014dussan@gmail.com>
2025-04-06 23:14:09 -0500

    feat(copilot): add configuration for zbirenbaum/copilot.lua plugin

--- lua/plugins/copilots.lua
+++ lua/plugins/copilots.lua
@@ -1,8 +1,29 @@
 return {
+  -- {
+  --   "supermaven-inc/supermaven-nvim",
+  --   config = function()
+  --     require("supermaven-nvim").setup({})
+  --   end,
+  -- },
   {
-    "supermaven-inc/supermaven-nvim",
-    config = function()
-      require("supermaven-nvim").setup({})
-    end,
+    "zbirenbaum/copilot.lua",
+    event = "BufReadPost",
+    opts = {
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        hide_during_completion = true,
+        debounce = 10,
+        trigger_on_accept = true,
+        keymap = {
+          accept = "<TAB>",
+          accept_word = false,
+          accept_line = false,
+          next = "<M-]>",
+          prev = "<M-[>",
+          dismiss = "<C-c>",
+        },
+      },
+    },
   },
 }
