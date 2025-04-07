6a5450f74754614659d6358772a1f772d44eec65
Daniel Dussan <daniel2014dussan@gmail.com>
2025-04-06 20:01:26 -0500

    feat(python): enabled venv-selector.nvim plugin

---
+++ lua/plugins/python.lua
@@ -0,0 +1,12 @@
+return {
+  "linux-cultist/venv-selector.nvim",
+  enabled = true,
+  opts = {
+    name = "venv",
+    notify_user_on_venv_activation = true,
+    search_venv_managers = true,
+    search = true,
+    dap_enabled = true,
+    parents = 2,
+  },
+}
